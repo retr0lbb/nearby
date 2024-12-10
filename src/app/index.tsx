ac4023cfcc4982b1dd204be03af7327ce9bb8b72
retr0lbb <retr0lbb@gmail.com>
2024-12-10 16:34:26 -0300

    starting app

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export default function Index() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text style={{ fontSize: 32 }}>Hello React Native</Text>
+    </View>
+  )
+}
